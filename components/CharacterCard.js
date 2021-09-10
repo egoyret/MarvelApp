efc6020fe5226da67a4be8de687361031755e781
Enrique Goyret <78978458+egoyret@users.noreply.github.com>
2021-09-10 16:12:18 -0300

    Initial load

---
+++ components/CharacterCard.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const styles = StyleSheet.create({
+  container: {
+		flexDirection: 'row', 
+		alignItems: 'center', 
+		marginLeft: 20,
+		marginRight: 20,
+		marginTop: 5,
+		borderWidth: 2,
+		padding: 10,
+		borderColor: 'darkred',
+		borderRadius: 10,
+		backgroundColor: 'rgb(223, 184, 184)'
+  },
+	image: {
+		width: 30,
+		height: 30,
+		marginRight: 10
+	},
+	font: {
+		fontWeight: 'bold'
+	}
+});
+
+
+export default function CharacterCard({id, image, name}) {
+  const navigation = useNavigation();
+  // console.log('CharacterCard image ',image);
+  return (
+   
+   <TouchableOpacity style={styles.container} onPress={() => navigation.navigate('Detail', {id:  id})}>
+     <Image style={styles.image} source={{uri: image}}/>
+     <Text style={styles.font}>{name}</Text>
+   </TouchableOpacity>
+
+);
+}
+
